2ce916e204d1f01a846925e3828386a9e80bee26
yaroshenko.g <yaroshenko270@gmail.com>
2016-08-03 11:45:21 +0300

    fix ways

--- my-auth/2/src/app.routes.ts
+++ my-auth/2/src/app.routes.ts
@@ -3,12 +3,12 @@ import { Home } from './home';
 import { Login } from './login';
 import { Signup } from './signup';
 import { AuthGuard } from './common/auth.guard';
-import { Log } from './startPage';
+import { StartPage } from './startPage';
 
 export const routes: RouterConfig = [
   { path: '',       component:  Login },
   { path: 'login',  component: Login },
-  { path: 'logins',  component: Log },
+  { path: 'startPage',  component: StartPage },
   { path: 'signup', component: Signup },
   { path: 'home',   component: Home, canActivate: [AuthGuard] },
   { path: '**',     component: Login },
